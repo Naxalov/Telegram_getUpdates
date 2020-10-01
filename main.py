055a53c71e9e3e7c4eb1013a375b5acb905f477b
Zarif MAC <uzbzarif@gmail.com>
2020-10-01 19:16:20 +0900

    idle

--- main.py
+++ main.py
@@ -18,21 +18,31 @@ def sendMessage(chat_id, text):
     response = requests.get(url=url, params=parameter)
 
 
+def getUpdates():
+    url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+
+    response = requests.get(url=url)
+
+    data = response.json()['result']
+    chat_id = []
+    for i in data:
+        chat_id.append(i['message']['chat']['id'])
+
+    # message = data['message']
+    # chat = data['message']['chat']
+    # from1 = data['message']['from']
+    # text = data['message']['text']
+    # chat_id = chat['id']
+    return chat_id
+
+
 TOKEN = '1324065101:AAF23TNO8EXW_JT6bdXo7bDOxfa3agGvJNI'
 
 
-# url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
-
-# response = requests.get(url=url)
-
-# data = response.json()['result'][0]
-# message = data['message']
-# chat = data['message']['chat']
-# from1 = data['message']['from']
-# text = data['message']['text']
 # pprint(text)
 i = 0
 while True:
-    chat_id = 86775091
+    chat_id = getUpdates()
     i += 1
-    sendMessage(chat_id, f'Salom:{i}')
+    for idx in chat_id:
+        sendMessage(idx, f'Salom:{i}')

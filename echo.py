d46bef4ff0b6b50d92a9f38c8ad8b872cfecde00
Zarif MAC <uzbzarif@gmail.com>
2020-10-01 19:51:21 +0900

    echo 1

---
+++ echo.py
@@ -0,0 +1,51 @@
+import requests
+from pprint import pprint
+
+# Hard code request
+# response = requests.get('https://api.telegram.org/bot187988593:AAH2bUUIAgGDvlnXlNshjD6cNoGVg4qGZLE/getMe')
+
+# print(response.text)
+
+# More clean code
+
+
+def sendMessage(chat_id, text):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    parameter = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    response = requests.get(url=url, params=parameter)
+
+
+def getUpdates():
+    url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+
+    response = requests.get(url=url)
+
+    data = response.json()['result']
+    chat_id = []
+    update_id = []
+    # update_id = update['result'][-1]['update_id']
+    for i in data:
+        chat_id.append(i['message']['chat']['id'])
+        # update_id.append(i[])
+
+    # message = data['message']
+    # chat = data['message']['chat']
+    # from1 = data['message']['from']
+    # text = data['message']['text']
+    # chat_id = chat['id']
+    return chat_id
+
+
+TOKEN = '1324065101:AAF23TNO8EXW_JT6bdXo7bDOxfa3agGvJNI'
+
+
+# pprint(text)
+i = 0
+while True:
+    chat_id = getUpdates()
+    i += 1
+    for idx in chat_id:
+        sendMessage(idx, f'Salom:{i}')
---
+++ echo_1.py
@@ -0,0 +1,48 @@
+import requests
+from pprint import pprint
+
+# Hard code request
+# response = requests.get('https://api.telegram.org/bot187988593:AAH2bUUIAgGDvlnXlNshjD6cNoGVg4qGZLE/getMe')
+
+# print(response.text)
+
+# @tutorial_1bot
+TOKEN = '1324065101:AAF23TNO8EXW_JT6bdXo7bDOxfa3agGvJNI'
+
+
+def getUpdates():
+    url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+    response = requests.get(url=url)
+    if response.status_code:
+        update = response.json()
+
+    return update
+
+
+def get_message(update):
+    txt = update['result'][-1]['message']['text']
+    update_id = update['result'][-1]['update_id']
+    return txt, update_id
+
+
+def sendMessage(id, txt):
+
+    parameter = {
+        'chat_id': id,
+        'text': txt,
+        'disable_notification': True,
+    }
+
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    response = requests.get(url=url, params=parameter)
+
+
+last_update_id = -5
+while True:
+    x = getUpdates()
+    txt, update_id = get_message(x)
+    print(f'update_id: {update_id} last_id:{last_update_id}')
+    if last_update_id != update_id:
+        sendMessage(86775091, txt)
+        last_update_id = update_id
